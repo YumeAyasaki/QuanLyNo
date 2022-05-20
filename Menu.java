0e13b4f8507cd0a0b1b11aea6b173e668634b540
ducquang2 <nducquangg@gmail.com>
2022-05-20 11:52:50 +0700

    them menu (tam thoi)

---
+++ Menu.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Menu {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Boolean loop = true;
+        int choice;
+
+        System.out.println("|*-----------------------------------------*|");
+        System.out.println("|*Option 1:                                *|");
+        System.out.println("|*Option 2:                                *|");
+        System.out.println("|*Option 3:                                *|");
+        System.out.println("|*Option 4: Thoat.                         *|");
+        System.out.println("|*-----------------------------------------*|");
+
+        do {
+            choice = scan.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Chon 1");
+                    break;
+            
+                case 2:
+                    System.out.println("Chon 2");
+                    break;
+                
+                case 3:
+                    System.out.println("Chon 3");
+                    break;
+
+                case 4:
+                    loop = false;
+                    System.out.println("Thoat");
+                    break;
+
+                default:
+                    System.out.println("Chon lai");
+                    break;
+            }
+        } while (loop);
+        scan.close();
+    }
+}
